7c6ede04336ae87b61b77139b3e12ec331dbe3cb
Nithin Rao Koluguri <nithinraok@users.noreply.huggingface.co>
2024-01-02 17:22:12 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,138 @@
+from nemo.collections.asr.models import EncDecRNNTBPEModel
+import gradio as gr
+from pydub import AudioSegment
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+MODEL_NAME="nvidia/parakeet-rnnt-1.1b"
+
+
+def get_transcripts(audio_path):
+
+    model = EncDecRNNTBPEModel.from_pretrained(model_name="nvidia/parakeet-rnnt-1.1b").to(device)
+    model.eval()
+    text = model.transcribe([audio_path])[0][0]
+    return text
+
+article = (
+    "<p style='text-align: center'>"
+    "<a href='https://huggingface.co/nvidia/parakeet-rnnt-1.1b' target='_blank'>🎙️ Learn more about Parakeet model</a> | "
+    "<a href='https://arxiv.org/abs/2305.05084' target='_blank'>📚 FastConformer paper</a> | "
+    "<a href='https://github.com/NVIDIA/NeMo' target='_blank'>🧑‍💻 Repository</a>"
+    "</p>"
+)
+examples = [
+    ["data/conversation.wav"],
+    ["data/id10270_5r0dWxy17C8-00001.wav"],
+]
+
+def _return_yt_html_embed(yt_url):
+    video_id = yt_url.split("?v=")[-1]
+    HTML_str = (
+        f'<center> <iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe>'
+        " </center>"
+    )
+    return HTML_str
+
+def download_yt_audio(yt_url, filename):
+    info_loader = youtube_dl.YoutubeDL()
+    
+    try:
+        info = info_loader.extract_info(yt_url, download=False)
+    except youtube_dl.utils.DownloadError as err:
+        raise gr.Error(str(err))
+    
+    file_length = info["duration_string"]
+    file_h_m_s = file_length.split(":")
+    file_h_m_s = [int(sub_length) for sub_length in file_h_m_s]
+    
+    if len(file_h_m_s) == 1:
+        file_h_m_s.insert(0, 0)
+    if len(file_h_m_s) == 2:
+        file_h_m_s.insert(0, 0)
+    file_length_s = file_h_m_s[0] * 3600 + file_h_m_s[1] * 60 + file_h_m_s[2]
+    
+    if file_length_s > YT_LENGTH_LIMIT_S:
+        yt_length_limit_hms = time.strftime("%HH:%MM:%SS", time.gmtime(YT_LENGTH_LIMIT_S))
+        file_length_hms = time.strftime("%HH:%MM:%SS", time.gmtime(file_length_s))
+        raise gr.Error(f"Maximum YouTube length is {yt_length_limit_hms}, got {file_length_hms} YouTube video.")
+    
+    ydl_opts = {"outtmpl": filename, "format": "worstvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"}
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            ydl.download([yt_url])
+        except youtube_dl.utils.ExtractorError as err:
+            raise gr.Error(str(err))
+
+
+def yt_transcribe(yt_url, task, max_filesize=75.0):
+    html_embed_str = _return_yt_html_embed(yt_url)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        filepath = os.path.join(tmpdirname, "video.mp4")
+        download_yt_audio(yt_url, filepath)
+        audio = AudioSegment.from_file(filepath)
+        wav_filepath = os.path.join(tmpdirname, "audio.wav")
+        audio.export(wav_filepath, format="wav")
+
+    text = get_transcripts(wav_filepath)
+    return html_embed_str, text
+
+
+demo = gr.Blocks()
+
+mf_transcribe = gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.inputs.Audio(source="microphone", type="filepath", optional=True)
+    ],
+    outputs="text",
+    layout="horizontal",
+    theme="huggingface",
+    title="Parakeet RNNT 1.1B: Transcribe Audio",
+    description=(
+        "Transcribe microphone or audio inputs with the click of a button! Demo uses the"
+        f" checkpoint [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and NVIDIA NeMo to transcribe audio files"
+        " of arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+file_transcribe = gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.inputs.Audio(source="upload", type="filepath", optional=True, label="Audio file"),
+    ],
+    outputs="text",
+    layout="horizontal",
+    theme="huggingface",
+    title="Parakeet RNNT 1.1B: Transcribe Audio",
+    description=(
+        "Transcribe microphone or audio inputs with the click of a button! Demo uses the"
+        f" checkpoint [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and NVIDIA NeMo to transcribe audio files"
+        " of arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+yt_transcribe = gr.Interface(
+    fn=yt_transcribe,
+    inputs=[
+        gr.inputs.Textbox(lines=1, placeholder="Paste the URL to a YouTube video here", label="YouTube URL"),
+    ],
+    outputs=["html", "text"],
+    layout="horizontal",
+    theme="huggingface",
+    title="Parakeet RNNT 1.1B: Transcribe Audio",
+    description=(
+        "Transcribe microphone or audio inputs with the click of a button! Demo uses the"
+        f" checkpoint [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and NVIDIA NeMo to transcribe audio files"
+        " of arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+with demo:
+    gr.TabbedInterface([mf_transcribe, file_transcribe], ["Microphone", "Audio file"])
+
+demo.launch(enable_queue=True)
